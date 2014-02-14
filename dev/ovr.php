bf4d9d8640166afb0a18eb60b1dde7ca770edd81
Cydrobolt <summermontreal@gmail.com>
2014-02-14 11:48:03 -0500

    commented out ovr so it won't created problems

--- dev/ovr.php
+++ dev/ovr.php
@@ -3,7 +3,7 @@
 //ERRORS COULD OCCUR IF YOU DO NOT CONFIGURE POLR'S REQ.PHP CONFIG CORRECTLY IN PRODUCTION
 //Polr CONFIGURATION
 
-
+/*
 $host = "localhost"; //Enter Mysql host address
 $user = "root"; //Mysql user
 $passwd = ""; //Mysql user password
@@ -11,3 +11,4 @@ $db = "polr"; //Mysql DB name
 $wsa = "localhost/polr/php"; //Address of website : e.g polr.cf - do not include http://
 $ip = $_SERVER['REMOTE_ADDR']; //How Polr should fetch the user's ip - some hosts require you to use getenv()
 $ovr = 1;
+*/
