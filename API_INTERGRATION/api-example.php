d2a0f89b8a4940027b5258b5e12d49a40c8f5e33
Cydrobolt <summermontreal@gmail.com>
2014-04-08 20:47:41 -0400

    api intergration examples

---
+++ API_INTERGRATION/api-example.php
@@ -0,0 +1,15 @@
+//PolrLib Example
+
+include('polrlib.php');
+
+$polr = new polr("insert api key here");
+
+$toshorten = "http://google.com";
+
+$shortened = $polr->shorten($toshorten);
+
+$tolookup = "1";
+
+$lookedup = $polr->lookup($tolookup);
+
+echo $toshorten."<br>".$tolookup."<br>".$shortened."<br>".$lookedup;
