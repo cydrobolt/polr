8d994f3ea4104b89bfe0d8862451d6a66ff2a325
Cydrobolt <summermontreal@gmail.com>
2014-04-14 16:41:03 -0400

    About update

--- about.php
+++ about.php
@@ -28,9 +28,11 @@
                     <li><a href="about.php">About</a></li><li><a href="contact.php">Contact</a></li>
                 </ul>
                 <ul class="nav pull-right navbar-nav">
-                    <?php include('polrauth.php');
+                    <?php
+                    include('polrauth.php');
                     $polrauth = new polrauth();
-                    $polrauth->headblock(); ?>
+                    $polrauth->headblock();
+                    ?>
                     <li><a href="register.php">Sign Up</a></li>
                     <li class="divider-vertical"></li>
                     <li class="dropdown">
@@ -46,7 +48,7 @@
                             </form>
                         </div>
                     </li>
-<?php $polrauth->headendblock(); ?>
+                    <?php $polrauth->headendblock(); ?>
 
                 </ul>
             </div>
@@ -54,11 +56,38 @@
         <div class="container">
             <div class="jumbotron" style="text-align:center; padding-top:80px; background-color: rgba(0,0,0,0);">
                 <h1>Polr</h1><br><p>Polr is an <a href='http://en.wikipedia.org/wiki/Open_source'>open source</a> URL shortener developed by <a href='http://cydrobolt.com'>Cydrobolt</a>. <br>If you would like to contribute, please join us at #polr on irc.freenode.net:6667 (<a href='http://webchat.freenode.net/?channels=#polr'>Webchat</a>)<br>If you would like to use Polr on your own website, visit the Polr <a href='//project.polr.cf'>project website</a>. <br>The code is hosted at Github - <a href='http://github.com/Cydrobolt/polr'>here</a></p>
+                <br><h2>Why Polr?</h2><br>
+                <div class="col-md-4"><b>Simple and minimalistic</b>
+                    <p>Polr is developed with minimalism and simplicity in mind. Unlike many other URL Shorteners, 
+                        Polr is free of obstructing ads, excess text, and extravagant pictures or alerts.
+                    </p>
+                </div>
+                <div class="col-md-4"><b>Reliable and powerful</b>
+                    <p>Polr is hosted on Red Hat's powerful infrastructure. Though we do not guarantee uptime, Polr's minimalistic philosophy and capable infrastructure enables users to shorten and access links with a low latency.</p>
+                </div>
+                <div class="col-md-4"><b>Actually short URLs</b>
+                    <p>While many URL shorteners claim to 'shorten' your URLs, the URLs they create are often longer than 11 characters. Polr strives to create truly short URLs.</p>
+                </div>
             </div>
-
-            <footer>
-                <p>&copy; Copyright 2014 Polr - <a href='privacypolicy.php'>Privacy Policy</a> - <a href='tos.php'>Terms of Service</a></p>
-            </footer>
         </div>
-    </body>
+        <br>
+        <div class="container jumbotron" style='text-align: center'>
+            <div class="col-md-4"><b>Tinyurl.com</b><br>example.com/example -> tinyurl.com/3xsrg5</div>
+            <div class="col-md-4"><b style='color:green'>Polr.cf</b><br>example.com/example -> polr.cf/6l</div>
+            <div class="col-md-4"><b>Bit.ly</b><br>example.com/example -> bit.ly/1gcaB14</div>
+
+        </div>
+        <div style='padding-left: 8%'>
+            <hr>
+            <span style='font-size: 75%'>
+                Bitly &trade; is a trademark owned by Bitly Inc., Tinyurl &trade; is a trademark owned by Gilby Productions. All trademarks are owned by their respective owners. Polr is not in any way affiliated with Gilby Productions or Bitly Inc.
+            </span>    
+            <br><br>
+            <span style='font-size: 75%'>
+                <p>&copy; Copyright 2014 Polr - <a href='privacypolicy.php'>Privacy Policy</a> - <a href='tos.php'>Terms of Service</a></p>
+            </span>
+
+        </div>
+    </div>
+</body>
 </html>
