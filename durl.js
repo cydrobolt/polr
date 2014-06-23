71531d94b6797e41d1f74738d6622d26bef020da
Chaoyi Zha <summermontreal@gmail.com>
2014-06-23 12:59:15 -0400

    uglify

--- durl.js
+++ durl.js
@@ -1,13 +1,1 @@
-function z(t) {
-    return atob(t);
-}
-$(function() {
-    function y (a) {
-        return z(a);
-    }
-    var var1 = $("#j").val();
-    var var2 = y(var1);
-    var var5 = $("#k").val();
-    var var4 = y(var5);
-    $("#i").val("http://"+var4+"/"+var2);
-});
+function y(a){return z(a)}function z(a){return atob(a)}function r(a){return btoa(a)}function f(a){var b=y(a);return r(b)}$(function(){var a=$("#j").val(),b=y(a),c=$("#k").val(),d=y(c),e=y(z(y(y(f(f("V1ZWb1UwMUhUa1ZpTTFwTlpIb3dPUT09")))))),g=y(f(y(y(z(z(y("Vm10V2IyRnJPVmRSYkVwUlZrUkJPUT09")))))));$("#i").val(e+d+g+b)});
