469843f6b700a0cc9f8caa188a9ef942d6ffa320
Chaoyi Zha <summermontreal@gmail.com>
2014-11-07 18:14:21 -0500

    Security patches

--- req.php
+++ req.php
@@ -2,10 +2,16 @@
 
 @(require_once('config.php'));
 include('version.php');
-$debug = 1; // Set to 1 in order to enable debug mode (shows sensitive database info), use for troubleshooting
+$debug = 0; // Set to 1 in order to enable debug mode (shows sensitive database info), use for troubleshooting
 $footer = "&copy; Copyright 2014 $wsn. Powered by <a href='http://github.com/cydrobolt/polr'>Polr</a> ver $version build $reldate";
 //connect to mysql with $mysqli variable
 $mysqli = new mysqli($host, $user, $passwd, $db) or $wp = 1; //If cannot connect, then set var $wp to 1
+// Attempt to set Charset as UTF8 to avoid real_escape_string vulnerabilities
+if (!$mysqli->set_charset("utf8")) {
+    $insecure = TRUE;
+} else {
+    $insecure = FALSE;
+}
 
 function autoloader($class) {
         include $class . '.php';
@@ -66,7 +72,7 @@ function showerror() {
 	//Show an error, and die. If Debug is on, show SQL error message
     global $debug;
     global $mysqli;
-    echo "There seems to be a problem :'( *sniff* . Click > <a href='http://webchat.freenode.net/?channels=polr'>here</a> contact an administrator.";
+    echo "There seems to be a problem :'( *sniff* . Contact an administrator to report this issue.";
     if ($debug == 1) {
         echo "<br>Error:<br>";
         echo $mysqli->error;
