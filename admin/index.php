3215d5f22a2e3cf343eca88b2a9c83d3db7be2cd
Chaoyi Zha <summermontreal@gmail.com>
2014-11-08 15:38:12 -0500

    password changing

--- admin/index.php
+++ admin/index.php
@@ -108,12 +108,15 @@ if (!is_array($auth->islogged())) {
   <div class="tab-pane" id="links"><br>' . $linkshtml . '</div>
 
   '.$msges.'
-  <div class="tab-pane" id="settings"><br><b>Settings:</b><br><br><form action="ucp-settings.php" method="POST">
-  Current Password: <input type="password" name="cpw" /><br>New Password: <input type="password" name="npw" />
-  <br><br>Theme: <select name="theme"><option value="cosmo">Aurora (Default)</option><option value="flatly">Light Slopes</option>
-  <option value="yeti">Metro Shift</option><option value="darkly">Dark Slopes</option></select><br><input type="hidden" value="setchg" name="setchg" />
-  <br><br><input type="submit" class="btn-success btn-lg" disabled="disabled"/>
-</form></div>';
+  <div class="tab-pane" id="settings"><br>
+  <h3>Change password</h3>
+<form action=\'ucp-settingsp.php\' method=\'POST\'>
+    <input type=\'hidden\' name=\'action\' value=\'changepw\' />
+    Old Password: <input type=\'password\' name=\'currpw\' />
+    New Password: <input type=\'password\' name=\'newpw\' />
+    <input type=\'submit\' class=\'btn btn-success\'/>
+</form>
+  </div>';
     if ($isadmin == true) {
         $sessiondump = "";
         foreach ($_SESSION as $sevar) {
---
+++ ucp-settingsp.php
@@ -0,0 +1,49 @@
+<?php
+require_once 'req.php';
+if (!$_SESSION['li']) {
+    header('Location: index.php');
+}
+require_once 'polrauth.php';
+$polrauth = new polrauth();
+$islogged = $polrauth->islogged();
+$action = $mysqli->real_escape_string($_POST['action']);
+$username = $mysqli->real_escape_string($_SESSION['username']);
+if ($action == 'changepw') {
+    $currpw = $mysqli->real_escape_string($_POST['currpw']);
+    $newpw = $mysqli->real_escape_string($_POST['newpw']);
+
+    require_once 'password.php';
+    function noMc($length = 23) {
+        return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+    }
+    $salt = noMc();
+    $opts = array(
+        'cost' => 10,
+        'salt' => $salt
+    );
+    $hashed = password_hash($newpw, PASSWORD_BCRYPT, $opts);
+    $sqr = "SELECT `password` FROM `auth` WHERE `username`='{$username}';";
+    $res = $mysqli->query($sqr);
+    $fetch = mysqli_fetch_assoc($res);
+    $hpw = $fetch['password'];
+    $islegit = $polrauth->processlogin($username, $currpw);
+    if (!$islegit) {
+        die('Invalid current password. <a href="forgotpass.php">Forgot your password?</a>  <a href="ucp.php">Back</a>');
+    }
+
+    $sqr = "UPDATE auth SET password = '{$hashed}' WHERE `username`='{$username}';";
+    $res = $mysqli->query($sqr);
+    if ($res) {
+        require_once 'header.php';
+        echo "Success! <a href='ucp.php'>Back</a>";
+        require_once 'footer.php';
+        die();
+    } else {
+        require_once 'header.php';
+        echo "Error! <a href='ucp.php'>Back</a>";
+        require_once 'footer.php';
+        die();
+    }
+}
+echo "Invalid Action";
+die();
