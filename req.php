d071ae5159d78c33bbaf435316b87f63c2c0043d
Chaoyi Zha <summermontreal@gmail.com>
2014-11-08 15:11:03 -0500

    more security updates (slowly but surely) and htaccess update

--- req.php
+++ req.php
@@ -20,31 +20,43 @@ function autoloader($class) {
 spl_autoload_register('autoloader');
 session_start();
 
-function sqlrun($query) {
-    global $mysqli;
-    $queryrs = $query;
-    $resultrs = $mysqli->query($queryrs) or showerror();
-    return true;
-}
-
 function sqlex($table, $rowf, $where, $wval) {
-	// Check whether a certain row/column exists
-	// Look at SQLFetch for variable def's
     global $mysqli; //Import var into function
 //Sanitize strings
     $rowfs = $mysqli->real_escape_string($rowf);
     $tables = $mysqli->real_escape_string($table);
     $wheres = $mysqli->real_escape_string($where);
     $wvals = $mysqli->real_escape_string($wval);
-
-    $query = "SELECT $rowfs FROM $tables WHERE $wheres='$wvals'";
-    $result = $mysqli->query($query) or showerror();
+    $q2p = "SELECT {$rowfs} FROM {$tables} WHERE ?=?";
+	$stmt = $mysqli->prepare($q2p);
+	$stmt->bind_param('ss', $wheres, $wvals);
+	$stmt->execute();
+	$result = $stmt->get_result();
     $numrows = $result->num_rows;
     if (!$numrows) {
         return false;
     } else {
         return true;
     }
+}
+
+function sqlfetch($table, $rowf, $where, $wval) {
+    global $mysqli;
+
+    $rowfs = $mysqli->real_escape_string($rowf);
+    $tables = $mysqli->real_escape_string($table);
+    $wheres = $mysqli->real_escape_string($where);
+    $wvals = $mysqli->real_escape_string($wval);
+
+    //$query = "SELECT $rowfs FROM $tables WHERE $wheres='$wvals'";
+    $q2p = "SELECT {$rowfs} FROM {$tables} WHERE ?=?";
+	$stmt = $mysqli->prepare($q2p);
+	echo $mysqli->error;
+	$stmt->bind_param('ss', $wheres, $wvals);
+	$stmt->execute();
+	$result = $stmt->get_result();
+    $row = mysqli_fetch_assoc($result);
+    return $row[$rowf];
 }
 function sqlfetch($table, $rowf, $where, $wval) {
 	/*
@@ -72,7 +84,7 @@ function showerror() {
 	//Show an error, and die. If Debug is on, show SQL error message
     global $debug;
     global $mysqli;
-    echo "There seems to be a problem :'( *sniff* . Contact an administrator to report this issue.";
+    echo "There seems to be a problem. Contact an administrator to report this issue.";
     if ($debug == 1) {
         echo "<br>Error:<br>";
         echo $mysqli->error;
