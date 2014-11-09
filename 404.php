8116bcb2bda5d821d068ce4af7d35d02ee4e9e6f
Chaoyi Zha <summermontreal@gmail.com>
2014-11-09 13:38:12 -0500

    Give the 404 page a revamp

--- 404.php
+++ 404.php
@@ -1,29 +1,4 @@
 <!-- polr 404 -->
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>404 Polr</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <link rel="stylesheet" href="bootstrap.css"/>
-            <link rel="stylesheet" href="main.css"/>
-            <link rel="shortcut icon" href="favicon.ico">
-        </head>
-        <body style="padding-top:60px">
-            <div class="navbar navbar-inverse navbar-fixed-top">
-                <a class="navbar-brand" href="index.php">Polr</a>
-                <ul class="nav navbar-nav">
-                    <li><a href="//github.com/Cydrobolt/polr">Github</a></li>
-                    <li><a href="//project.polr.cf">Source</a></li>
-                    <li><a href="about.php">About</a></li><li><a href="contact.php">Contact</a></li>
-                </ul>
-            </div>
-            <div class="container">
-                <div class="jumbotron" style="text-align:center; padding-top:80px; background-color: rgba(0,0,0,0);">
+<?php require_once('header.php');?>
                     <h1>404</h1><br><h2>You step in the stream</h2><h2>But the water has moved on.</h2><h2>This page is not here</h2></div>
-            </div>
-                       <footer>
-                <p>&copy; Copyright 2013 Polr - Special Thanks to <a href="http://mywot.com">WOT</a></p>
-            </footer>
-    </body>
-</html>
+<?php require_once('footer.php');?>
