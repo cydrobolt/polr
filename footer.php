dc7982711db6fba217aa9c3d18b90e2e5c0f6531
Chaoyi Zha <summermontreal@gmail.com>
2014-11-17 18:27:06 -0500

    Comment footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div>            
             <footer>
-                <p id="footer-pad">&copy; Copyright <?php require_once('version.php');echo $relyear;?> The Polr Project. Proudly powered by <a href="//github.com/cydrobolt/polr">Polr</a></p>
+                <?php require_once('config.php');if($hidefooter==true){echo "<!--"}?><p id="footer-pad">&copy; Copyright <?php require_once('version.php');echo $relyear;?> The Polr Project. Proudly powered by <a href="//github.com/cydrobolt/polr">Polr</a></p> <?php require_once('config.php');if($hidefooter==true){echo "-->"}?>
             </footer>
         </div>
     </body>
