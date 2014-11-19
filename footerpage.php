6105c3b0fc6eaf0b6684212f0d2c4e3daa18b4c6
Chaoyi Zha <summermontreal@gmail.com>
2014-11-18 20:14:21 -0500

    add no footer option

--- footerpage.php
+++ footerpage.php
@@ -1,5 +1,6 @@
-</div>            
-<p id="footer-pad">&copy; Copyright <?php require_once('version.php');echo $relyear;?> The Polr Project. Proudly powered by <a href="//github.com/cydrobolt/polr">Polr</a></p>
+</div>
+<?php require_once('config.php');if($hidefooter==true){echo "<!--"}?><p id="footer-pad">&copy; Copyright <?php require_once('version.php');echo $relyear;?> The Polr Project. Proudly powered by <a href="//github.com/cydrobolt/polr">Polr</a></p> <?php require_once('config.php');if($hidefooter==true){echo "-->"}?>        
+
         </div>
     </body>
 </html>
