18b1afe00492766d3d60503e0e8e9833c845cc57
Chaoyi Zha <summermontreal@gmail.com>
2014-11-20 16:17:33 -0500

    Hide footer on Index

--- index.php
+++ index.php
@@ -132,7 +132,7 @@ if(!@include('config.php')) {
             </div>
             <div id="polrfooter">
             <footer>
-                <p id="footer-pad">&copy; Copyright <?php require_once('version.php');echo $relyear;?> The Polr Project. Proudly powered by <a href="//github.com/cydrobolt/polr">Polr</a></p>
+                <?php require_once('config.php');if($hidefooter==true){echo "<!--";}?><p id="footer-pad">&copy; Copyright <?php require_once('version.php');echo $relyear;?> The Polr Project. Proudly powered by <a href="//github.com/cydrobolt/polr">Polr</a></p> <?php require_once('config.php');if($hidefooter==true){echo "-->";}?>
             </footer>
             </div>
         </div>
