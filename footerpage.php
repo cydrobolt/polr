23b2af392bfb966eeab691945b716e8351b78556
Chaoyi Zha <summermontreal@gmail.com>
2014-11-20 16:16:57 -0500

    Fix semicolon placement

--- footerpage.php
+++ footerpage.php
@@ -1,5 +1,5 @@
 </div>
-<?php require_once('config.php');if($hidefooter==true){echo "<!--"}?><p id="footer-pad">&copy; Copyright <?php require_once('version.php');echo $relyear;?> The Polr Project. Proudly powered by <a href="//github.com/cydrobolt/polr">Polr</a></p> <?php require_once('config.php');if($hidefooter==true){echo "-->"}?>        
+<?php require_once('config.php');if($hidefooter==true){echo "<!--";}?><p id="footer-pad">&copy; Copyright <?php require_once('version.php');echo $relyear;?> The Polr Project. Proudly powered by <a href="//github.com/cydrobolt/polr">Polr</a></p> <?php require_once('config.php');if($hidefooter==true){echo "-->";}?>        
 
         </div>
     </body>
