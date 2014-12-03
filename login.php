f4afba7761e2c42958da806c47d5ab4385534124
Chaoyi Zha <summermontreal@gmail.com>
2014-12-02 20:12:44 -0500

    Use relative path in login.php form
    Thanks DrAwesome for pointing this out

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ echo '
   <h1>Login</h1><br/><br/>
   <div class="col-md-2"></div>
   <div class="col-md-8">
-    <form action="/loginproc.php" method="POST"><b>Username:</b><br/>
+    <form action="loginproc.php" method="POST"><b>Username:</b><br/>
       <input type="text" name="username" id="username" class="form-control"/><br/><b>Password:</b><br/>
       <input type="password" name="password" id="password" class="form-control"/><br />
       <input id="remember_me" style="padding-botton: 15px" type="checkbox" name="remember_me" value="remember_me" size="30" /> <b>Remember Me</b>
