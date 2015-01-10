bcdb22904c42237894139c6cd1d3c72bf78f6715
Chaoyi Zha <summermontreal@gmail.com>
2015-01-09 21:40:02 -0500

    Add inverse header back

--- admin/header.php
+++ admin/header.php
@@ -31,7 +31,7 @@
 </head>
 <body style="padding-top:60px">
     <div class="container-fluid">
-        <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
+        <nav role="navigation" class="navbar navbar-inverse navbar-fixed-top">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
