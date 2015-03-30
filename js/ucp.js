af2419f9465741d72f9a84dcbd3846375e25abe3
Chaoyi Zha <summermontreal@gmail.com>
2015-03-30 17:28:29 -0400

    Add settings tabbing

--- js/ucp.js
+++ js/ucp.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
     $('#tabsb').tab();
+    var url = document.location.toString();
+    if (url.match('#')) {
+        $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+    } 
+    
+    // Change hash for page-reload
+    $('.nav-tabs a').on('shown', function (e) {
+        window.location.hash = e.target.hash;
+    });
 });
 
 function dodisable(baseval) {
