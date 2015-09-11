6bf524c01b4927439ed18ec09881dd54f8b977a3
Chaoyi Zha <summermontreal@gmail.com>
2015-09-11 18:26:55 -0400

    turn off debug mode

--- lib-core.php
+++ lib-core.php
@@ -16,7 +16,7 @@
 */
 @(require_once('config.php'));
 include('version.php');
-$debug = 1; // Set to 1 in order to enable debug mode (shows sensitive database info), use for troubleshooting
+$debug = 0; // Set to 1 in order to enable debug mode (shows sensitive database info), use for troubleshooting
 $footer = "&copy; Copyright 2014 $wsn. Powered by <a href='http://github.com/cydrobolt/polr'>Polr</a> ver $version build $reldate";
 $hidefooter = true; // Let's hide this for now
 //connect to mysql with $mysqli variable
