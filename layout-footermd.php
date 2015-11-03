eb85f2466e39d0217f374bed7e0a71fbe3bc895f
Chaoyi Zha <summermontreal@gmail.com>
2015-11-02 23:11:25 -0500

    Clean directory for 2.0

--- layout-footermd.php
+++
@@ -1,7 +0,0 @@
-</div>
-        </div>
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-        <script src="js/index.js"></script>
-    </body>
-
-</html>
--- version.php
+++
@@ -1,4 +0,0 @@
-<?php
-$version = "1.4.1 Stable: Diligent Bison";
-$reldate = "June 13 2015";
-$relyear = 2015;
