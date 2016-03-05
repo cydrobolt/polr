aa2641be1378fbd71bb15e2d2f95e96a6da11769
Chaoyi Zha <summermontreal@gmail.com>
2016-03-05 00:12:03 -0500

    Classes cannot start with numbers

--- database/migrations/2016_03_05_043127_1x_to_2x_users.php
+++ database/migrations/2016_03_05_043127_1x_to_2x_users.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class 1xTo2xUsers extends Migration
+class Migrate1xTo2xUsers extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2016_03_05_043132_1x_to_2x_links.php
+++ database/migrations/2016_03_05_043132_1x_to_2x_links.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class 1xTo2xLinks extends Migration
+class Migrate1xTo2xLinks extends Migration
 {
     /**
      * Run the migrations.
