4ec9bc646249143af053a7d2179df63a9f40a78c
Chaoyi Zha <summermontreal@gmail.com>
2016-04-16 17:22:59 -0400

    Throttle anonymous API users, add a default quota

--- app/Helpers/ApiHelper.php
+++ app/Helpers/ApiHelper.php
@@ -16,11 +16,21 @@ class ApiHelper {
 
         $user = UserHelper::getUserByUsername($username);
 
+        if ($user) {
+            $api_quota = $user->api_quota;
+        }
+        else {
+            // TODO add option to change default quota for anonymous
+            // API users
+
+            $api_quota = 5;
+        }
+
         $links_last_minute = Link::where('is_api', 1)
             ->where('creator', $username)
             ->where('created_at', '>=', $last_minute)
             ->count();
 
-        return ($user->api_quota > -1 && $links_last_minute >= $user->api_quota);
+        return ($api_quota > -1 && $links_last_minute >= $api_quota);
     }
 }
