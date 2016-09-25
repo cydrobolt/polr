e7436f589fca4989d64fb12c068de9e7b56b9b91
RdN <cold_name@o2.pl>
2016-09-25 20:18:03 +0200

    Forcing no debug while tests

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,7 +11,7 @@
 |
 */
 
-$factory->define(App\User::class, function ($faker) {
+$factory->define(\App\Models\User::class, function ($faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
