2926409f135a7162d8852721f3cc46a72be5d5aa
Chaoyi Zha <summermontreal@gmail.com>
2016-10-11 22:47:43 -0400

    Output a helpful message in the JS console

--- public/js/base.js
+++ public/js/base.js
@@ -10,5 +10,10 @@ function esc_selector(selector) {
     return selector.replace( /(:|\.|\[|\]|,)/g, "\\$1" );
 }
 
+// Output helpful console message
+console.log('%cPolr', 'font-size:5em;color:green');
+console.log('%cNeed help? Open a ticket: https://github.com/cydrobolt/polr', 'color:blue');
+console.log('%cDocs: https://docs.polr.me', 'color:blue');
+
 // Set up Angular module
 var polr = angular.module('polr',[]);
