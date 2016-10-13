fcf1ffb324db4615860ee2f8e27dd48bc05855d0
Christian Dinse <github@dreiundsiebzig.eu>
2016-10-13 18:53:24 +0200

    Create statistic.blade.php
    The static view "statistic" file. Will completed soon.

---
+++ resources/views/statistic.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.base')
+
+@section('css')
+<link rel='stylesheet' href='/css/effects.css' />
+@endsection
+
+@section('content')
+<div>
+    Link statistic follows here.
+</div>
+@endsection
+
+@section('js')
+@endsection
