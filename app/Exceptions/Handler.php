474e1fb831af68d9b14c1abc3ab7bd957e09cf55
Juan Carlos Alonso <me@jcalonso.com>
2016-10-30 12:14:58 +0000

    Fixes testRequestGetNotExistShortUrl test
    This was changing the behaviour of the application based on the
    APP_DEBUG flag. I removed this and instead added logs

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,12 +3,13 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Support\Facades\Response;
 
-class Handler extends ExceptionHandler {
+class Handler extends ExceptionHandler
+{
     /**
      * A list of the exception types that should not be reported.
      *
@@ -23,7 +24,7 @@ class Handler extends ExceptionHandler {
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $e
+     * @param  \Exception $e
      * @return void
      */
     public function report(Exception $e)
@@ -34,34 +35,36 @@ class Handler extends ExceptionHandler {
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception $e
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $e)
     {
-        if (env('APP_DEBUG') != true) {
-            // Render nice error pages if debug is off
-            if ($e instanceof NotFoundHttpException) {
-                if (env('SETTING_REDIRECT_404')) {
-                    // Redirect 404s to SETTING_INDEX_REDIRECT
-                    return redirect()->to(env('SETTING_INDEX_REDIRECT'));
-                }
-                // Otherwise, show a nice error page
-                return view('errors.404');
+        if ($e instanceof NotFoundHttpException) {
+            if (env('SETTING_REDIRECT_404')) {
+                // Redirect 404s to SETTING_INDEX_REDIRECT
+                return redirect()->to(env('SETTING_INDEX_REDIRECT'));
             }
-            if ($e instanceof HttpException) {
-                $status_code = $e->getStatusCode();
-                $status_message = $e->getMessage();
+            // Otherwise, show a nice error page
+            return view('errors.404');
+        }
+        if ($e instanceof HttpException) {
+            $status_code = $e->getStatusCode();
+            $status_message = $e->getMessage();
+            Log::critical("$status_code error: $status_message");
 
-                if ($status_code == 500) {
-                    // Render a nice error page for 500s
-                    return view('errors.500');
-                }
-                else {
-                    // If not 500, render generic page
-                    return response(view('errors.generic', ['status_code' => $status_code, 'status_message' => $status_message]), $status_code);
-                }
+            if ($status_code == 500) {
+                // Render a nice error page for 500s
+                return view('errors.500');
+            } else {
+                // If not 500, render generic page
+                return response(
+                    view('errors.generic', [
+                        'status_code' => $status_code,
+                        'status_message' => $status_message
+                    ]),
+                    $status_code);
             }
         }
 
