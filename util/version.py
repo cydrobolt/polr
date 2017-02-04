2bd5cfd6122b6f29ad5b250c7145af07ceb1272d
Chaoyi Zha <summermontreal@gmail.com>
2017-02-04 00:06:17 -0500

    Update doc badges in versioning script

--- util/version.py
+++ util/version.py
@@ -1,7 +1,10 @@
 import re, datetime, string, random
 
-print "New version name (e.g 2.0.0):"
+print "> New version name (e.g 2.0.0):"
 new_version = raw_input()
+
+print "> Is this a stable release? [y, n]"
+is_stable = raw_input()
 
 with open('.env.setup', 'r+') as setup_env:
     setup_env_lines = setup_env.read()
@@ -12,7 +15,7 @@ with open('.env.setup', 'r+') as setup_env:
     # Update setup key
     setup_env_lines = re.sub(r'(?is)APP_KEY=[^\n]+', 'APP_KEY={}'.format(new_setup_key), setup_env_lines)
     # Update date and release in setup env
-    setup_env_lines = re.sub(r'(?is)VERSION=[0-9a-zA-Z\.]+', 'VERSION={}'.format(new_version), setup_env_lines)
+    setup_env_lines = re.sub(r'cVERSION=[0-9a-zA-Z\.]+', 'VERSION={}'.format(new_version), setup_env_lines)
     setup_env_lines = re.sub(r'(?is)VERSION_RELMONTH=\w+', 'VERSION_RELMONTH={}'.format(now.strftime('%B')), setup_env_lines)
     setup_env_lines = re.sub(r'(?is)VERSION_RELDAY=\w+', 'VERSION_RELDAY={}'.format(now.day), setup_env_lines)
     setup_env_lines = re.sub(r'(?is)VERSION_RELYEAR=\w+', 'VERSION_RELYEAR={}'.format(now.year), setup_env_lines)
@@ -21,5 +24,15 @@ with open('.env.setup', 'r+') as setup_env:
     setup_env.seek(0)
     setup_env.write(setup_env_lines)
     setup_env.truncate()
+
+if is_stable == 'y':
+    with open('docs/index.md', 'r+') as index_md:
+        index_md_lines = index_md.read()
+        index_md_lines = re.sub(r'(?is)stable-[0-9\.]+-green.svg', 'stable-{}-green.svg'.format(new_version), index_md_lines)
+
+        # Overwite existing file
+        index_md.seek(0)
+        index_md.write(index_md_lines)
+        index_md.truncate()
 
 print "Done!"
