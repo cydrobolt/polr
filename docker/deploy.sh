aafb4eb2b3dd7f99611af09ef4ea312a34fc0ac8
Programster <sdpagent@gmail.com>
2017-05-17 21:55:30 +0100

    Adding changes so that we store state in a volume and create the env file from the template on first deployment

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+if ! [ -n "$BASH_VERSION" ];then
+    echo "this is not bash, calling self with bash....";
+    SCRIPT=$(readlink -f "$0")
+    /bin/bash $SCRIPT
+    exit;
+fi
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd $HOME
+mkdir -p polr-data/storage
 
 # Remove any existing polr container
 docker kill polr-container
@@ -8,4 +19,5 @@ docker rm polr-container
 docker run -d \
 -p 80:80 \
 --name polr-container \
+--volume $HOME/polr-data:/data \
 polr
---
+++ docker/startup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /var/www/polr/
+
+if ! [ -f /data/env ]; then
+    cat /var/www/polr/.env.setup > /data/env
+fi
+
+ln -s /data/env .env
+ln -s /data/storage /var/www/polr/.
+
+mkdir -p /data/storage/app
+mkdir -p /data/storage/logs
+mkdir -p /data/storage/framework/cache
+mkdir -p /data/storage/framework/sessions
+mkdir -p /data/storage/framework/views
+
+# Set permissions/ownership
+chown root:www-data -R /var/www/polr
+chmod 750 -R /var/www/polr
+
+chown root:www-data -R /data
+chmod 770 -R /data
+
+
+# finish up by starting supervisor in the foreground to keep the container running
+/usr/bin/supervisord
