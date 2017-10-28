762eb0e06cc50c26f73fb856a568f1846bce6938
Anh Tuấn <mrsugarvn@gmail.com>
2017-10-28 20:35:38 +0700

    Copy to clipboard and Open in new tab features

--- public/js/shorten_result.js
+++ public/js/shorten_result.js
@@ -2,9 +2,17 @@ function select_text() {
     window.getSelection().selectAllChildren(document.getElementById('result-box'));
 }
 
+function copy_to_clipboard(){
+    var result_to_clipboard = new Clipboard('.result-box');
+    result_to_clipboard.on('success', function(){
+        toastr.success('Copied to clipboard', 'Success');
+        result_to_clipboard.destroy();
+    });
+}
+
 $(function () {
-    select_text();
     $('.result-box').click(function(){
         select_text();
+        copy_to_clipboard();
     });
 });
--- resources/views/shorten_result.blade.php
+++ resources/views/shorten_result.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <h3>Shortened URL</h3>
-<div id='result-box' class='result-box'>{{$short_url}}</div>
+<div id='result-box' class='result-box' data-clipboard-target='.result-box'>{{$short_url}}</div>
 <div class='shortened-control'>
     <a href='{{$short_url}}' target='_blank' class='btn btn-success'>Open in new tab</a>
     <a href='{{route('index')}}' class='btn btn-info'>Shorten another</a>
@@ -14,5 +14,6 @@
 @endsection
 
 @section('js')
+<script src='/js/clipboard.min.js'></script>
 <script src='/js/shorten_result.js'></script>
 @endsection
