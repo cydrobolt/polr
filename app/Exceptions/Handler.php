c0ce7d4d6eeeac57c3dda926741fa006f2da7cb2
Chaoyi Zha <summermontreal@gmail.com>
2018-02-11 17:05:57 -0500

    Use proper HTTP response code for pretty 500 page; reduce line length

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -60,11 +60,15 @@ class Handler extends ExceptionHandler {
 
                 if ($status_code == 500) {
                     // Render a nice error page for 500s
-                    return view('errors.500');
+                    return response(view('errors.500'), 500);
                 }
                 else {
                     // If not 500, render generic page
-                    return response(view('errors.generic', ['status_code' => $status_code, 'status_message' => $status_message]), $status_code);
+                    return response(
+                        view('errors.generic', [
+                            'status_code' => $status_code,
+                            'status_message' => $status_message
+                        ]), $status_code);
                 }
             }
             if ($e instanceof ApiException) {
