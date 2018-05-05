c136f0dec62770c851033a16a3bca706f665af41
abhishek jain <abhij89@gmail.com>
2018-05-06 05:13:05 +0530

    minor updates: adding space, changing error code, preserving old format for api stats response

--- app/Http/Controllers/Api/ApiAnalyticsController.php
+++ app/Http/Controllers/Api/ApiAnalyticsController.php
@@ -1,30 +1,32 @@
 <?php
-namespace App\Http\Controllers\Api;
-use Illuminate\Http\Request;
 
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
 use App\Helpers\LinkHelper;
 use App\Helpers\UserHelper;
 use App\Helpers\StatsHelper;
 use App\Exceptions\Api\ApiException;
 
 class ApiAnalyticsController extends ApiController {
-    public function lookupLinkStats (Request $request, $stats_type=false) {
+
+    public function lookupLinkStats(Request $request, $stats_type = false) {
         $user = $request->user;
-        $response_type = $request->input('response_type') ?: 'json';
+        $response_type = $request->input('response_type') ? : 'json';
 
         if ($user->anonymous) {
             throw new ApiException('AUTH_ERROR', 'Anonymous access of this API is not permitted.', 401, $response_type);
         }
 
         if ($response_type != 'json') {
-            throw new ApiException('JSON_ONLY', 'Only JSON-encoded data is available for this endpoint.', 401, $response_type);
+            throw new ApiException('JSON_ONLY', 'Only JSON-encoded data is available for this endpoint.', 400, $response_type);
         }
 
         $validator = \Validator::make($request->all(), [
-            'url_ending' => 'required|alpha_dash',
-            'stats_type' => 'required',
-            'left_bound' => 'date',
-            'right_bound' => 'date'
+                    'url_ending' => 'required|alpha_dash',
+                    'stats_type' => 'required',
+                    'left_bound' => 'date',
+                    'right_bound' => 'date'
         ]);
 
         if ($validator->fails()) {
@@ -35,7 +37,7 @@ class ApiAnalyticsController extends ApiController {
         $stats_type = $request->input('stats_type');
         $left_bound = $request->input('left_bound');
         $right_bound = $request->input('right_bound');
-        
+
         $stats_type_arr = explode(",", $stats_type);
 
         // ensure user can only read own analytics or user is admin
@@ -46,34 +48,46 @@ class ApiAnalyticsController extends ApiController {
         }
 
         if (($link->creator != $user->username) &&
-                !(UserHelper::userIsAdmin($user->username))){
+                !(UserHelper::userIsAdmin($user->username))) {
             // If user does not own link and is not an admin
             throw new ApiException('ACCESS_DENIED', 'Unauthorized.', 401, $response_type);
         }
 
         try {
             $stats = new StatsHelper($link->id, $left_bound, $right_bound);
-        }
-        catch (\Exception $e) {
+        } catch (\Exception $e) {
             throw new ApiException('ANALYTICS_ERROR', $e->getMessage(), 400, $response_type);
         }
 
         $fetched_stats = [];
-        
-        if (in_array('day',$stats_type_arr)) {
-            $fetched_stats['day'] = $stats->getDayStats();
+
+        if (in_array('day', $stats_type_arr)) {
+            if (count($stats_type_arr) == 1) {
+                $fetched_stats = $stats->getDayStats();
+            } else {
+                $fetched_stats['day'] = $stats->getDayStats();
+            }
         }
-        if (in_array('country',$stats_type_arr)) {
-            $fetched_stats['country'] = $stats->getCountryStats();
+        if (in_array('country', $stats_type_arr)) {
+            if (count($stats_type_arr) == 1) {
+                $fetched_stats = $stats->getCountryStats();
+            } else {
+                $fetched_stats['country'] = $stats->getCountryStats();
+            }
         }
-        if (in_array('referer',$stats_type_arr)) {
-            $fetched_stats['referer'] = $stats->getRefererStats();
+        if (in_array('referer', $stats_type_arr)) {
+            if (count($stats_type_arr) == 1) {
+                $fetched_stats = $stats->getRefererStats();
+            } else {
+                $fetched_stats['referer'] = $stats->getRefererStats();
+            }
         }
-        
+
 
         return self::encodeResponse([
-            'url_ending' => $link->short_url,
-            'data' => $fetched_stats,
-        ], 'data_link_' . $stats_type, $response_type, false);
+                    'url_ending' => $link->short_url,
+                    'data' => $fetched_stats,
+                        ], 'data_link_' . $stats_type, $response_type, false);
     }
+
 }
