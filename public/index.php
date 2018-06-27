9aad504e555f4014c5561a9265c9bd944459e292
Serhii Chaplia <serg.chaplya@gmail.com>
2018-06-27 13:25:01 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -11,6 +11,10 @@
 |
 */
 
+if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+    $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+}
+
 $app = require __DIR__.'/../bootstrap/app.php';
 
 /*
