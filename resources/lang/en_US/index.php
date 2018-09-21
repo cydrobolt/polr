be48d2b0de6790a11fbc66ab33c7512467f7491f
klezm <20573323+klezm@users.noreply.github.com>
2018-09-21 19:00:37 +0200

    fix

--- resources/lang/en_US/index.php
+++ resources/lang/en_US/index.php
@@ -1,10 +1,12 @@
 <?php
 
-return array(
-    'public'            => 'public',
-    'private'           => 'private',
-    'checkavailability' => 'check availability',
-    'linkoptions'       => 'link options',
-    'short'             => 'shorten',
-    'loadingtips'       => 'Loading tips...'
-);
+    return array(
+        'public'            => 'public',
+        'private'           => 'private',
+        'checkavailability' => 'check availability',
+        'linkoptions'       => 'link options',
+        'short'             => 'shorten',
+        'loadingtips'       => 'Loading tips...'
+    );
+
+?>
