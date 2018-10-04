5a1a0879c2169ed96f61b0edf31f0975cb435352
PhraseApp <support@phraseapp.com>
2018-10-04 11:36:07 +0200

    Updated locale file about.php

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -1,18 +1,16 @@
 <?php
-
-    return [
-
-        'buildinfo' => [
-            'title'         => 'Build Information',
-            'version'       => 'Version: :ver',
-            'release'       => 'Release date: :reldate',
-            'appinstall'    => 'App Install: :appname on :appaddress on :genat',
-            'admin'         => 'You are seeing the information above because you are logged in as an administrator.',
-        ],
-        'about' => [
-            'powered'       => ':app is powered by Polr 2, an open source, minimalist link shortening platform.',
-            'learnmore'     => 'Learn more at <a href="https://github.com/Cydrobolt/polr">its GitHub page</a> or its <a href="//project.polr.me">project site</a>.',
-            'license'       => 'Polr is licensed under the GNU GPL License.',
-        ],
-        'moreinfo' => 'More Information',
-    ];
+return array(
+	"about" => array(
+		"learnmore" => "Learn more at <a href=\"https://github.com/Cydrobolt/polr\">its GitHub page</a> or its <a href=\"//project.polr.me\">project site</a>.",
+		"license" => "Polr is licensed under the GNU GPL License.",
+		"powered" => ":app is powered by Polr 2, an open source, minimalist link shortening platform.",
+	),
+	"buildinfo" => array(
+		"admin" => "You are seeing the information above because you are logged in as an administrator.",
+		"appinstall" => "App Install: :appname on :appaddress on :genat",
+		"release" => "Release date: :reldate",
+		"title" => "Build Information",
+		"version" => "Version: :ver",
+	),
+	"moreinfo" => "More Information",
+);
