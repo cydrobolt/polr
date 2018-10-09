366bdd7bcecf7a90d83ea123187fea4a8f71bb33
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-09 13:50:11 +0200

    Removed use App\Http\Controllers\Controller
    Removed use App\Http\Controllers\Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
 
 class Controller extends BaseController {
